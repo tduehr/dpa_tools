64f03dc2d5640ec0aa69988fc69fe9807eef87bf
tduehr <tduehr@gmail.com>
2013-11-21 11:05:44 -0600

    inital ruby tools

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ source 'https://rubygems.org'
 gem 'serialport', '~>1.1.0'
 gem 'airby', '~>0.1.0', git: 'git://github.chi.matasano.com/td/airby.git'
 gem 'ds1102d', '~>0.1.0', git: 'git://github.chi.matasano.com/td/ds1102d.git'
+group :development do
+  gem 'yard', '~>0.8.7'
+  gem "bundler", "~> 1.0"
+  gem "jeweler", "~> 1.8.7"
+  gem 'rubygems-bundler', '~> 1.4.2'
+end
---
+++ lib/fetcher.rb
@@ -0,0 +1,29 @@
+class Fetcher
+  def initialize resource: nil, dev: nil, baud: 9600, data_bits: 8, parity: 0, stop_bits: 1
+    dev ||= Dir.glob('/dev/tty.uart*').first
+    @scope = Ds1102d.new resource
+    @scope.open @scope.session.find_resources('USB*').first unless resource
+    @serial = SerialPort.open dev, baud, data_bits, stop_bits, parity
+  end
+
+  def connect
+    @scope.open @scope.address || @scope.session.find_resources('USB*').first unless @scope.connected?
+  end
+
+  def get count = 1
+    arr = count.times.map do |x|
+      hsh = {}
+      hsh[:time_scale] = @scope.timebase.scale
+      hsh[:time_offset] = @scope.timebase.offset
+      hsh[:chan1_scale] = @scope.channel(1).scale
+      hsh[:chan1_offset] = @scope.channel(1).offset
+      hsh[:chan2_scale] = @scope.channel(2).scale
+      hsh[:chan2_offset] = @scope.channel(2).offset
+      hsh[:chan2_data] = @scope.waveform.data 'chan2'
+      hsh[:chan1_data] = @scope.waveform.data 'chan1'
+      hsh[:dig_data] = @scope.waveform.data 'digital'
+      hsh[:sampling_rate] = @scope.acquire.sampling_rate
+    end
+    count < 2 ? arr.first : arr
+  end
+end
